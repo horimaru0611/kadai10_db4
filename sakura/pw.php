87bcacb5dd39a93d9162f9ed7391ace5d2a31de0
horimaru0611 <horihori0611rh@gmail.com>
2024-09-24 00:42:56 +0900

    Delete sakura directory

--- sakura/pw.php
+++
@@ -1,6 +0,0 @@
-<?php 
-$id  = "test1";
-//ハッシュ化
-$lpw = password_hash($id, PASSWORD_DEFAULT); 
-echo $lpw;
-
--- sakura/s1.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-$_SESSION["name"] = "yamazaki";
-$_SESSION["age"] = "40";
-
-echo session_id();
